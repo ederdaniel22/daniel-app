49b371194b6cae3e83c7427862126479629bb913
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-07-28 22:43:57 -0400

    Atualização de pastas e corrigindo rotas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Menu from './components/Menu';
+import Menu from '../../components/Menu';
 
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
     return ( 
         <div style={{ backgroundColor: "#141414" }}>
             <Menu />
@@ -50,4 +50,4 @@ function App() {
     </div>
   );
 }
-export default App;
+export default Home;
---
+++ src/pages/cadastro/Video/index.jsx
@@ -0,0 +1,11 @@
+import React from 'React';
+
+function CadastroVideo() {
+    return ( 
+        <div>
+        Página de Cadastro de Video 
+        </div>
+    )
+}
+
+export default CadastroVideo;
