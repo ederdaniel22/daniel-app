4252e18fbda5465930b5c8cb925d5ffd41a0bd4b
ederdaniel22 <60990935+ederdaniel22@users.noreply.github.com>
2020-07-30 22:58:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-server.listen(port,() => {
-  //eslint-disable-next-line no-console
-  console.log(`JSON Server is running in ${port}`);
+server.listen(port, () => {
+    //eslint-disable-next-line no-console
+    console.log(`JSON Server is running in ${port}`);
 });
